5b0a89c451701c61242df4eecec059334578606b
Bruno Daniel Rios Cosser <bruno_rioscosser18_2314@hotmail.com>
2023-06-21 23:29:17 -0500

    Prueba

--- src/main/java/com/example/Cl03DeliveryPostgrePaisApplication.java
+++ src/main/java/com/example/Cl03DeliveryPostgrePaisApplication.java
@@ -1,9 +1,10 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class Cl03DeliveryPostgrePaisApplication {
 
 	public static void main(String[] args) {
